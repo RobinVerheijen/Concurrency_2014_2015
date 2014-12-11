b915281457c8d4bce250edee47e1f92bc1ecec38
Saron Grave <iphone.grave@gmail.com>
2014-12-11 12:46:45 +0100

    opdracht 2 startup

---
+++ Opdracht_2/src/Apl.java
@@ -0,0 +1,16 @@
+/**
+ * Main class with the static void main method which is called after starting the application.
+ * @author Robin Verheijen
+ * @author Saron Grave
+ */
+public class Apl {
+	
+	/**
+	 * Main method where can be chosen which assignment should be tested.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	    
+	}
+
+}
